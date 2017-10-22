af24f366bf0af9bb4ab9614c936c861e85cce2d6
Felix Geisendörfer <felix@debuggable.com>
2017-10-22 12:07:02 +0200

    add missing file

---
+++ cmd/netfix/http.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"net/http"
+	"sort"
+	"time"
+
+	ndb "github.com/felixge/netfix/db"
+)
+
+func serveHttp(c Config, db *sql.DB) error {
+	server := &http.Server{
+		Addr:         c.HttpAddr,
+		Handler:      OutagesHandler(db),
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 10 * time.Second,
+	}
+	return server.ListenAndServe()
+}
+
+func OutagesHandler(db *sql.DB) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		f := ndb.OutageFilter{
+			MinLoss:        0.01,
+			OutageLoss:     0.01,
+			OutageDuration: 2 * time.Minute,
+			OutageGap:      5 * time.Minute,
+		}
+		fmt.Sscan(r.URL.Query().Get("min_loss"), &f.MinLoss)
+		fmt.Sscan(r.URL.Query().Get("outage_loss"), &f.OutageLoss)
+		if d, err := time.ParseDuration(r.URL.Query().Get("outage_duration")); err == nil {
+			f.OutageDuration = d
+		}
+		if d, err := time.ParseDuration(r.URL.Query().Get("outage_gap")); err == nil {
+			f.OutageGap = d
+		}
+
+		outages, err := ndb.Outages(db, f)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			fmt.Fprintf(w, "%s\n", err)
+			return
+		}
+
+		fmt.Fprintf(w, "%s\n", f)
+		fmt.Fprintf(w, "%d outages (%s):\n\n", len(outages), outages.Duration())
+		sort.Slice(outages, func(i, j int) bool {
+			return outages[i].Start.After(outages[j].Start)
+		})
+		for _, o := range outages {
+			fmt.Fprintf(w, "%s\n", o)
+		}
+	})
+}
